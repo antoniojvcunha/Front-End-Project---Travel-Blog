ba6541cac39c7a4dd00fe93743e42d593c0993d7
Antonio Cunha <tojo98@live.com.pt>
2024-11-16 19:22:00 +0000

    update card container

--- Travel_Blog/src/App.jsx
+++ Travel_Blog/src/App.jsx
@@ -1,5 +1,8 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
+
+
 
 function App() {
 
@@ -8,6 +11,8 @@ function App() {
     <div>
       <Navbar/>
       <Hero/>
+      <Card/>
+      
     </div>
       
     </>
--- Travel_Blog/src/components/Hero.jsx
+++ Travel_Blog/src/components/Hero.jsx
@@ -1,10 +1,10 @@
+
 function Hero() {
     return(
         <>
         <div className="container flex items-center justify-center p-6 mx-auto mt-20">
             <p className="uppercase text-3xl">Adventures exists in the unknown</p>
-        </div>
-            
+        </div>  
         </>
     )
 }
