4adb2d0dd82434022994639fa846b71041b52366
Antonio Cunha <tojo98@live.com.pt>
2024-11-27 23:33:20 +0000

    starting locations page

--- Travel_Blog/src/views/Locations.jsx
+++ Travel_Blog/src/views/Locations.jsx
@@ -3,8 +3,13 @@ import Navbar from "../components/Navbar";
 function Locations() {
   return (
     <>
-      <div className="bg-ceu bg-cover min-h-screen text-black">
+      <div className="bg-ceu bg-cover min-h-screen text-black absolute left-0 right-0">
         <Navbar />
+        <img
+          src="/images/1.jpg"
+          alt=""
+          className="absolute left-0 right-0 w-full h-96 object-cover "
+        />
       </div>
     </>
   );
